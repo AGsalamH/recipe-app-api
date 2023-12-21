331c28d1fb06ca379070db2c4d71f771033aa389
Ahmed Gamal <agsalamh@gmail.com>
2023-12-21 13:37:38 +0200

    feat: add User model that represents project users

--- users/models.py
+++ users/models.py
@@ -1,1 +1,23 @@
 from django.db import models   # Noqa
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager
+)
+
+from core.models import BaseModel
+
+# Create your models here.
+
+
+class BaseUser(BaseModel, AbstractBaseUser):
+    class Meta:
+        abstract = True
+
+
+class User(BaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, validators=[])
+
+    USERNAME_FIELD = 'email'
+
+    objects = BaseUserManager
