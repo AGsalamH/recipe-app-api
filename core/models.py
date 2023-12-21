b8ef849fd6f2f9c83b4a48f75725236ce32d857d
Ahmed Gamal <agsalamh@gmail.com>
2023-12-21 13:34:48 +0200

    feat: Add timstamped base model for DB models

---
+++ core/models.py
@@ -0,0 +1,18 @@
+import uuid
+from django.db import models
+
+
+class BaseModel(models.Model):
+    '''Base class for database models'''
+    id = models.UUIDField(
+                            verbose_name='ID',
+                            primary_key=True,
+                            serialize=False,
+                            auto_created=True,
+                            default=uuid.uuid4
+                        )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
