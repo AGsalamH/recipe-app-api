695e5f188fb43db011165e4b3c9d3d3155588b61
Ahmed Gamal <agsalamh@gmail.com>
2024-04-29 20:35:28 +0300

    feat: add name to create user form

--- users/admin.py
+++ users/admin.py
@@ -36,7 +36,7 @@ class UserAdmin(BaseUserAdmin):
             None,
             {
                 "classes": ("wide",),
-                "fields": ("email", "password1", "password2"),
+                "fields": ("email", "name", "password1", "password2"),
             },
         ),
     )
