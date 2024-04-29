e5bba806eb46687710800e76991abc62d865c322
Ahmed Gamal <agsalamh@gmail.com>
2024-04-29 14:45:41 +0300

    feat: Modify UserAdmin model to work with our custom user model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,54 @@
-from django.contrib import admin # Noqa
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from users.models import User
 # Register your models here.
 
-admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    '''Define admin pages for users'''
+    list_display = ('email', 'name')
+    ordering = ('id',)
+    readonly_fields = ("created_at", "updated_at")
+
+    add_form_template = "admin/auth/user/add_form.html"
+    # change_user_password_template = None
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("name",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    # form = UserChangeForm
+    # add_form = UserCreationForm
+    # change_password_form = AdminPasswordChangeForm
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("email", "name")
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
+
+
+admin.site.register(User, UserAdmin)
