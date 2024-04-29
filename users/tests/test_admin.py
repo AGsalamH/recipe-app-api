4c5890adb54643d60042682ae3ca9da72af96740
Ahmed Gamal <agsalamh@gmail.com>
2024-04-29 14:44:16 +0300

    chore: fix code linting

--- users/tests/test_admin.py
+++ users/tests/test_admin.py
@@ -3,15 +3,14 @@ Tests for the Django Admin modifications.
 '''
 from django.urls import reverse
 from django.test import TestCase, Client
-from django.contrib import admin
 from django.contrib.auth import get_user_model
 
 
 class TestAdminSite(TestCase):
     '''Tests for Django Admin.'''
-    
+
     def setUp(self):
-        '''Create user and client.'''    
+        '''Create user and client.'''
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
@@ -25,7 +24,6 @@ class TestAdminSite(TestCase):
         )
         self.client.force_login(self.admin_user)
 
-    
     def test_users_list(self):
         '''Test that users are listed on page.'''
         response = self.client.get(reverse('admin:users_user_changelist'))
