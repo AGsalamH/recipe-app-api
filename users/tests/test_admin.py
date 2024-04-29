ab07132e8260b914063dc92fc38b635cc9670fa5
Ahmed Gamal <agsalamh@gmail.com>
2024-04-29 18:50:27 +0300

    test: user change page works

--- users/tests/test_admin.py
+++ users/tests/test_admin.py
@@ -29,3 +29,9 @@ class TestAdminSite(TestCase):
         response = self.client.get(reverse('admin:users_user_changelist'))
         self.assertContains(response, self.user.email)
         self.assertContains(response, self.user.name)
+
+    def test_user_change_page(self):
+        '''Test that user change page works'''
+        url = reverse('admin:users_user_change', args=(self.admin_user.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
