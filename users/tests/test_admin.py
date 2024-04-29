45226872ffbf024e1784e39a7863a19d96958207
Ahmed Gamal <agsalamh@gmail.com>
2024-04-29 14:36:20 +0300

    test: users are listed on admin page

---
+++ users/tests/test_admin.py
@@ -0,0 +1,33 @@
+'''
+Tests for the Django Admin modifications.
+'''
+from django.urls import reverse
+from django.test import TestCase, Client
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+
+class TestAdminSite(TestCase):
+    '''Tests for Django Admin.'''
+    
+    def setUp(self):
+        '''Create user and client.'''    
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            name='harry Potter',
+            password='sample123',
+        )
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            name='malfoy',
+            password='sample123',
+        )
+        self.client.force_login(self.admin_user)
+
+    
+    def test_users_list(self):
+        '''Test that users are listed on page.'''
+        response = self.client.get(reverse('admin:users_user_changelist'))
+        self.assertContains(response, self.user.email)
+        self.assertContains(response, self.user.name)
