fd7d925bfb3ad4d3e618f9d85b946f3051bda8a7
Ahmed Gamal <35201159+AGsalamH@users.noreply.github.com>
2024-05-02 15:18:45 +0300

    docs: update BaseModel docs [skip ci]

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    '''Base class for database models'''
+    '''Timestamped Base class for database models'''
     id = models.UUIDField(
                             verbose_name='ID',
                             primary_key=True,
