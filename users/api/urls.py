33bdbb0fbf331e745b375e495ce315b61973c501
Ahmed Gamal <agsalamh@gmail.com>
2024-05-03 18:51:26 +0300

    feat: add user profile endpoints

--- users/api/urls.py
+++ users/api/urls.py
@@ -2,7 +2,7 @@
 Users API endpoints.
 '''
 from django.urls import path
-from users.api.views import CreateUserView
+from users.api.views import CreateUserView, RetrieveUpdateProfileView
 from users.auth.views import ObtainToken
 
 
@@ -10,5 +10,5 @@ app_name = 'users_api'
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
     path('auth-token/', ObtainToken.as_view(), name='obtain-auth-token'),
-
+    path('<pk>/', RetrieveUpdateProfileView.as_view(), name='me'),
 ]
--- users/api/views.py
+++ users/api/views.py
@@ -3,10 +3,21 @@ Views for the users API.
 '''
 from django.contrib.auth import get_user_model
 from rest_framework import generics
-from users.api.serializers import UserRegisterationSerializer
+from rest_framework.permissions import IsAuthenticated
+from users.api.serializers import (
+    UserRegisterationSerializer,
+    UserProfileSerializer
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     '''Create a new user in the system.'''
     queryset = get_user_model().objects.all()
     serializer_class = UserRegisterationSerializer
+
+
+class RetrieveUpdateProfileView(generics.RetrieveUpdateAPIView):
+    '''Retrieve and modify profile data for a user.'''
+    queryset = get_user_model().objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = (IsAuthenticated,)
