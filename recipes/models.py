01523c6c3a594b0b4f68bc9064b603e617ebfd98
Ahmed Gamal <agsalamh@gmail.com>
2024-05-09 17:41:17 +0300

    feat: add field tags to recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from tags.models import Tag
 
 
 class Recipe(models.Model):
@@ -12,6 +13,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
 
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name='recipes')
 
     def __str__(self):
         '''String representation of a recipe.'''
