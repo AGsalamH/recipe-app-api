477259484a2a1a22909e7ea831fa5f4632e45023
Ahmed Gamal <agsalamh@gmail.com>
2024-05-20 21:08:31 +0300

    feat: create ingredient model

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,3 +1,16 @@
-from django.db import models  # Noqa
+from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Ingredient(models.Model):
+    '''Ingredients for recipes.'''
+    name = models.CharField(max_length=120)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
