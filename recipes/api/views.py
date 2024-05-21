fa0341d9b9ee4b20f1763a5823dcb9da6c8a0989
Ahmed Gamal <agsalamh@gmail.com>
2024-05-21 06:48:41 +0300

    feat: create and update recipes with ingredients

--- recipes/api/views.py
+++ recipes/api/views.py
@@ -5,17 +5,19 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
 from recipes.models import Recipe
-from recipes.api.serializers import TagRecipeSerializer
+from recipes.api.serializers import RecipeSerializer
 
 
 class RecipeViewSet(ModelViewSet):
     '''Recipes model CRUD viewset.'''
-    queryset = Recipe.objects.select_related('user').all()
-    serializer_class = TagRecipeSerializer
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user.id)
+        return self.queryset.filter(
+            user=self.request.user.id
+        ).select_related('user').prefetch_related('tags', 'ingredients')
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
